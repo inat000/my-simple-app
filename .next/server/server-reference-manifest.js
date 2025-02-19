13b39e8764728aba0c7b9a6a76e1b4a05e5a1536
inat000 <inattanon000@gmail.com>
2025-02-19 20:06:13 +0000

    build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1HmXIww+UXx5fl/SOTrr7TlVFgOgfh4ss6u5uEk0aQw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
